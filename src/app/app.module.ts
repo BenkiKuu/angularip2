f59a2813a7c3475bab234022a6faeb249f10383b
Leo Igane <iganeleo@gmail.com>
2018-06-06 20:27:10 +0300

    initiates the angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Angularip2Component } from './angularip2/angularip2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Angularip2Component
   ],
   imports: [
     BrowserModule
