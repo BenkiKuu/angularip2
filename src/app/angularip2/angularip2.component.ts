d2af4b21a2c4cc2743ad1263dda05b5ff151308a
Leo Igane <iganeleo@gmail.com>
2018-06-07 14:34:12 +0300

    creates the function for the repos

--- src/app/angularip2/angularip2.component.ts
+++ src/app/angularip2/angularip2.component.ts
@@ -7,12 +7,13 @@ import { Angularip2Service } from '../angularip2.service'
 })
 export class Angularip2Component implements OnInit {
   angularip2: any[];
-
+  angular1p2repo: any[];
   constructor(private angularip2Service: Angularip2Service) {
 this.angularip2Service.profileInfo().subscribe(angularip2 => {
   console.log(angularip2);
   this.angularip2 = angularip2;
 });
+this.angularip2Service.profileRepos().subscribe(angular1p2repo => {})
   }
 
   ngOnInit() {
