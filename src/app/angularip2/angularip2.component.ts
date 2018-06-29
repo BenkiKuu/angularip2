6b7118d351ae50d71e6ffc8ee565ff7afa82aa46
Leo Igane <iganeleo@gmail.com>
2018-06-30 02:01:14 +0300

    adds a container for the app

--- src/app/angularip2/angularip2.component.ts
+++ src/app/angularip2/angularip2.component.ts
@@ -8,12 +8,31 @@ import { Angularip2Service } from '../angularip2.service'
 export class Angularip2Component implements OnInit {
   angularip2: any[];
   angular1p2repo: any[];
+  username: string;
   constructor(private angularip2Service: Angularip2Service) {
-this.angularip2Service.profileInfo().subscribe(angularip2 => {
-  console.log(angularip2);
-  this.angularip2 = angularip2;
+    this.angularip2Service.profileInfo().subscribe(angularip2 => {
+      console.log(angularip2);
+      this.angularip2 = angularip2;
 });
-this.angularip2Service.profileRepos().subscribe(angular1p2repo => {})
+    this.angularip2Service.profileRepos().subscribe(angular1p2repo => {
+      console.log(angular1p2repo);
+      this.angular1p2repo = angular1p2repo;
+    })
+
+  }
+
+  findProfile(){
+    this.angularip2Service.updateProfile(this.username);
+    this.angularip2Service.profileInfo().subscribe(angularip2 => {
+      console.log(angularip2);
+      this.angularip2 = angularip2;
+});
+    this.angularip2Service.profileRepos().subscribe(angular1p2repo => {
+      console.log(angular1p2repo);
+      this.angular1p2repo = angular1p2repo;
+    })
+
+
   }
 
   ngOnInit() {
